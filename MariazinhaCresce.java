09ba6d4ff3dbec1c34d3f0ce8c37f5ca9f82e222
Ícaro Nogueira <79952012+paraguaio@users.noreply.github.com>
2021-03-18 20:39:54 -0300

    Create MariazinhaCresce.java

---
+++ MariazinhaCresce.java
@@ -0,0 +1,35 @@
+/*
+ * 3. Mariazinha tem 1,50m e cresce 2 centímetros por ano, 
+ *  enquanto Maricota tem 1,10m e cresce 3 centímetros por ano. 
+ *  Construir um programa que calcule e imprima quantos anos serão necessários para 
+ *  que Maricota seja maior que Mariazinha.
+ */
+package exerciciovlogica;
+
+public class MariazinhaCresce 
+
+
+{
+    public static void main(String[] args) 
+    {
+
+        int mariazinha = 150;
+        int maricota = 110;
+        int anos = 0;
+        
+        System.out.println("Quantos anos são necessários para que Maricota seja maior que Mariazinha? ");
+        
+        while ( mariazinha > maricota) 
+        {
+            mariazinha = mariazinha + 2;
+            maricota = maricota + 3;
+            anos = anos + 1;
+        
+        }    
+        
+        System.out.println("Serão necessários" + " " + anos + " " + "anos");   
+        
+    }
+    
+    
+}
